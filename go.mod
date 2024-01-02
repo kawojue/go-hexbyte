f54a05535f7517a10a64427048c95034988cbd63
Raheem Kawojue <kawojue08@gmail.com>
2024-01-02 01:12:53 +0100

    ..

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kawojue/go-hexbytes
+
+go 1.21.5
---
+++ hexbytes.go
@@ -0,0 +1,40 @@
+package hexbyte
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func GenerateRandomHexString(numBytes int) string {
+	randomBytes := make([]byte, numBytes)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return BytesToHex(randomBytes)
+}
+
+func GenerateRandomBytes(numBytes int) ([]byte, error) {
+	randomBytes := make([]byte, numBytes)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return randomBytes, nil
+}
+
+func HexToBytes(hexString string) []byte {
+	bytes, err := hex.DecodeString(hexString)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return bytes
+}
+
+func BytesToHex(data []byte) string {
+	return hex.EncodeToString(data)
+}
